c1112cf34e6bce7ff44fd5fbe0c56e0f2b9a3173
Sergey Sorokin <albuspersiv19@gmail.com>
2025-06-12 14:52:43 +0300

    feat: Проинициализирован nest-cli и fastify

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+// import {NestFactory} from '@nestjs/core';
+//
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(process.env.PORT ?? 3000);
+// }
