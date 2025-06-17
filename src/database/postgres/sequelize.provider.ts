c892cf782413e65acb059593f50b6a06f4ad6247
Sergey Sorokin <albuspersiv19@gmail.com>
2025-06-17 09:01:46 +0300

    feat: Подключение PosgreSQL

---
+++ src/database/postgres/sequelize.provider.ts
@@ -0,0 +1,20 @@
+import { Provider } from '@nestjs/common';
+import { Sequelize } from 'sequelize-typescript';
+import { appConfig } from '../../config';
+
+export const SEQUELIZE = 'SEQUELIZE';
+
+export const sequelizeProvider: Provider<Sequelize> = {
+  provide: SEQUELIZE,
+  useFactory: async (): Promise<Sequelize> => {
+    const sequelize: Sequelize = new Sequelize({
+      dialect: 'postgres',
+      logging: false,
+      ...appConfig.postgres,
+    });
+
+    await sequelize.authenticate();
+
+    return sequelize;
+  },
+};
